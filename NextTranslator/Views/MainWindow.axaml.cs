07138a7afb49742e5e34997954e71babbb798785
thanhtamlk8273 <92800064+thanhtamlk8273@users.noreply.github.com>
2024-06-01 16:14:10 +0700

    Little changes

--- NextTranslator/Views/MainWindow.axaml.cs
+++ NextTranslator/Views/MainWindow.axaml.cs
@@ -1,11 +1,44 @@
 using Avalonia.Controls;
+using Avalonia.ReactiveUI;
+using FluentAvalonia.UI.Controls;
+using NextTranslator.ViewModels;
+using ReactiveUI;
+using System;
+using System.Linq;
+using System.Reactive.Disposables;
 
 namespace NextTranslator.Views;
 
-public partial class MainWindow : Window
+public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
 {
     public MainWindow()
     {
         InitializeComponent();
+
+        this.WhenActivated(disposables =>
+        {
+            // Default NavView
+            if (this.FindControl<NavigationView>("BaseView") is NavigationView nv)
+            {
+                nv.SelectionChanged += OnBaseViewSelectionChanged;
+                nv.SelectedItem = nv.MenuItems.ElementAt(0);
+            }
+        });
+    }
+
+    private void OnBaseViewSelectionChanged(object? sender, NavigationViewSelectionChangedEventArgs e)
+    {
+        if (sender is not NavigationView nv) return;
+
+        if (e.IsSettingsSelected)
+        {
+            nv.Content = null;
+        }
+        else if (e.SelectedItem is NavigationViewItem nvi)
+        {
+            var smpPage = $"NextTranslator.Views.{nvi.Tag}";
+            nv.Content = (Type.GetType(smpPage) is Type type ? Activator.CreateInstance(type) : null);
+            nv.DataContext = (DataContext as MainWindowViewModel)?.ViewModel;
+        }
     }
 }
